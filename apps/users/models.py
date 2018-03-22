1afb7797858468f4a5c1f080bd7c8c93b5993622
qiyuan <qiyuan@mercenary.com.cn>
2018-03-22 18:09:43 +0800

    增加 设备信息表

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import AbstractUser
 
 
@@ -23,4 +24,34 @@ class ProfileInfo(AbstractUser):
         verbose_name_plural = verbose_name
 
     def __str__(self):
-        return self.username
+        return self.mobile
+
+
+class AppInfo(models.Model):
+    """
+    设备
+    """
+    GENDER_CHOICES = (
+        ('1', 'ios'),
+        ('2', 'android'),
+        ('3', '微信公众号'),
+        ('4', '微信小程序'),
+        ('5', '支付宝小程序'),
+        ('6', '快应用'),
+    )
+    deviceid = models.CharField(max_length=40, blank=False, verbose_name='设备ID',
+                                help_text='设备的唯一标识 mac地址 超过40位只保存40位')
+    model = models.CharField(max_length=20, blank=True, verbose_name='设备型号',
+                             help_text='类似于R9sPlus iPhone 6 等 超过20位只保存20位')
+    device_ver = models.CharField(max_length=10, blank=True, verbose_name='设备版本号',
+                                  help_text='设备的版本号 类似于 10.3.3 超过10位只保存10位')
+    channel_code = models.CharField(max_length=2, blank=False, verbose_name='注册渠道号',
+                                    help_text='注册渠道 目前只支持 ios 和android')
+    date_joined = models.DateTimeField(verbose_name='注册时间', default=timezone.now)
+
+    class Meta:
+        verbose_name = "设备"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.deviceid
