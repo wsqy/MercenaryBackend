6bd9cd1491bd7a4ed506567237109ea37f0a5de2
qiyuan <qiyuan@mercenary.com.cn>
2018-04-21 15:47:27 +0800

    choice改为int类型了

--- apps/order/models.py
+++ apps/order/models.py
@@ -7,25 +7,25 @@ class SubCategory(models.Model):
     订单二级分类
     """
     CLASSIFICATION_CHOICE = (
-        ('1', '即时'),
-        ('2', '顺风'),
-        ('3', '替身'),
-        ('4', '技能'),
-        ('5', '活动'),
+        (1, '即时'),
+        (2, '顺风'),
+        (3, '替身'),
+        (4, '技能'),
+        (5, '活动'),
     )
     TEMPLATE_CHOICE = (
-        ('1', '描述'),
-        ('2', '商品'),
-        ('3', '快递'),
+        (1, '描述'),
+        (2, '商品'),
+        (3, '快递'),
     )
     name = models.CharField(max_length=255, null=False, verbose_name='分类名', help_text='二级分类')
     weight = models.IntegerField(default=1, verbose_name='权重', help_text='权重',
                                  validators=IntRangeValidator())
     is_active = models.BooleanField(default=True, verbose_name='是否激活', help_text='是否激活')
-    classification = models.CharField(max_length=2, choices=CLASSIFICATION_CHOICE,
-                                      verbose_name='父分类', help_text='所属分类')
-    template = models.CharField(max_length=2, choices=TEMPLATE_CHOICE, default='1',
-                                verbose_name='分类模板', help_text='分类数据所使用的模板')
+    classification = models.IntegerField(choices=CLASSIFICATION_CHOICE, default=1,
+                                         verbose_name='父分类', help_text='所属分类')
+    template = models.IntegerField(choices=TEMPLATE_CHOICE, default=1,
+                                   verbose_name='分类模板', help_text='分类数据所使用的模板')
 
     def __str__(self):
         return self.name
