863196e29466b652fbcabe0a3bcb8e6abf43d8ed
qiyuan <qiyuan@mercenary.com.cn>
2018-04-24 10:06:36 +0800

    url双引号移除成单引号

--- MercenaryBackend/urls.py
+++ MercenaryBackend/urls.py
@@ -19,7 +19,7 @@ import xadmin
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 from rest_framework.documentation import include_docs_urls
-from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
+from rest_framework_jwt.views import refresh_jwt_token
 
 from users.views import DeviceRegisterViewset, SmsCodeViewset, UserViewset
 from utils.views import get_celery_task_status
@@ -28,18 +28,18 @@ from order.views import SubCategoryViewset, OrderViewSet
 
 router = DefaultRouter()
 
-router.register(r'users', UserViewset, base_name="users")
-router.register(r'devices', DeviceRegisterViewset, base_name="devices")
-router.register(r'codes', SmsCodeViewset, base_name="codes")
-router.register(r'district', DistrictViewset, base_name="district")
-router.register(r'category', SubCategoryViewset, base_name="category")
-router.register(r'order', OrderViewSet, base_name="order")
+router.register(r'users', UserViewset, base_name='users')
+router.register(r'devices', DeviceRegisterViewset, base_name='devices')
+router.register(r'codes', SmsCodeViewset, base_name='codes')
+router.register(r'district', DistrictViewset, base_name='district')
+router.register(r'category', SubCategoryViewset, base_name='category')
+router.register(r'order', OrderViewSet, base_name='order')
 
 urlpatterns = [
     url(r'^mercenary-admin/', xadmin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     # 文档
-    url(r'docs/', include_docs_urls(title="mercenary")),
+    url(r'docs/', include_docs_urls(title='mercenary')),
     # 刷新token
     url(r'^refresh_token/', refresh_jwt_token),
     # celery 查询任务状态
