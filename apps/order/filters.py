537165b5bf70f48ac65a9c05af52199fa1299799
qiyuan <qiyuan@mercenary.com.cn>
2018-05-10 18:41:05 +0800

    支持订单按照一级分类搜索

--- apps/order/filters.py
+++ apps/order/filters.py
@@ -12,7 +12,9 @@ class OrderFilter(django_filters.rest_framework.FilterSet):
     订单的过滤类
     """
     status__in = NumberInFilter(name='status', lookup_expr='in', help_text='订单状态范围')
+    classification = django_filters.NumberFilter(name='category__classification',
+                                                 help_text='一级分类')
 
     class Meta:
         model = OrderInfo
-        fields = ['deposit', 'status__in']
+        fields = ['deposit', 'status__in', 'classification']
