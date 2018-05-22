33b5c23a70f58114abe2f3269c3d8e46137f8aa7
qiyuan <qiyuan@mercenary.com.cn>
2018-05-22 17:58:13 +0800

    账户表中增加用户余额变动数据

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class BalanceDetail(models.Model):
+    """
+    账户余额详情
+    """
+    ORIGIN_TYPE_CHOICE = (
+        ('10', '跑腿单佣金'),
+        ('20', '招募令佣金'),
+        ('30', '提现'),
+    )
+
+    user = models.ForeignKey(User, verbose_name='用户', help_text='用户')
+
+    origin_type = models.CharField(verbose_name='变动类型', help_text='变动类型',
+                                   max_length=5, choices=ORIGIN_TYPE_CHOICE)
+
+    balance = models.IntegerField(default=0, verbose_name='金额', help_text='变动金额(分)')
+
+    remark = models.CharField(verbose_name='备注信息', blank=True, null=True,
+                              help_text='备注信息', max_length=128)
+
+    class Meta:
+        verbose_name = '账户余额变动表'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return '{}-{}'.format(self.origin_type, self.balance)
+
