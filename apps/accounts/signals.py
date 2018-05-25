90b8a67ae5331214d1ffe6950fd005b525a78bde
qiyuan <qiyuan@mercenary.com.cn>
2018-05-25 17:37:08 +0800

    关注银行卡绑定成功的信号,修改用户的真实姓名

--- apps/accounts/signals.py
+++ apps/accounts/signals.py
@@ -1,7 +1,11 @@
-from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import BalanceDetail
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+
+from .models import BalanceDetail, BankCard
 from users.models import ProfileExtendInfo
+
+User = get_user_model()
 
 
 @receiver(post_save, sender=BalanceDetail)
@@ -10,3 +14,11 @@ def balance_detail_insert(sender, instance=None, created=False, **kwargs):
         profile_extend_instance = ProfileExtendInfo.objects.get(user=instance.user)
         profile_extend_instance.balance += instance.balance
         profile_extend_instance.save()
+
+
+@receiver(post_save, sender=BankCard)
+def balance_detail_insert(sender, instance=None, created=False, **kwargs):
+    if created:
+        for user_one in User.objects.filter(id=instance.user.id):
+            user_one.first_name = instance.name
+            user_one.save()
