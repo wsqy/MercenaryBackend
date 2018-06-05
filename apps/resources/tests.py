911e107ff16e7068f1816258f9465fc6121317fe
qiyuan <qiyuan@mercenary.com.cn>
2018-06-05 17:13:46 +0800

    加入部分测试代码 暂时不作用

--- apps/resources/tests.py
+++ apps/resources/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class ResourceTests(APITestCase):
+    def setUp(self):
+        print('start.....')
+        response = self.client.get('http://118.24.157.119:8000/resource/', format='json')
+        print(response)
+        print(dir(response))
+        print(response.status_code)
+        print(response.data)
+        self.assertEqual(response.data.get('count'), 1)
+        print('start.....')
+
+    def test_count(self):
+        print('test.....')
+        print('test.....')
+
+    def tearDown(self):
+        print('end.....')
+        print('end.....')
--- apps/users/tests.py
+++ apps/users/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from rest_framework.test import APIRequestFactory
 
-# Create your tests here.
+
+class DevicesTests(TestCase):
+    def setUp(self):
+        print('start.....')
+        factory = APIRequestFactory()
+        request = factory.get('/devices/')
+        print(request)
+        print('start.....')
+
+    def test_count(self):
+        print('test.....')
+        print('test.....')
+
+    def tearDown(self):
+        print('end.....')
+        print('end.....')
