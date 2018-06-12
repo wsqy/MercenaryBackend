eafd0121912edeceaab6071071f259f8cc993129
qiyuan <qiyuan@mercenary.com.cn>
2018-06-12 11:40:34 +0800

    订单邮件通知修正为支付完成才有通知

--- apps/order/signals.py
+++ apps/order/signals.py
@@ -8,7 +8,7 @@ from .tasks import order_create_send_email_notice
 
 @receiver(post_save, sender=OrderInfo)
 def order_complete(sender, instance=None, created=False, **kwargs):
-    if created:
+    if instance.status == 11:
         order_create_send_email_notice.apply_async(args=('测试邮件到达:订单号--{}'.format(instance.id),))
     if instance.status == 50:
         # todo 监听订单状态为50后的
