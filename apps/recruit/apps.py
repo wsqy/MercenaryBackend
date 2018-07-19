eb31596eb65f533824ed494cc5e0d8e512d765fd
qiyuan <qiyuan@mercenary.com.cn>
2018-07-19 10:42:33 +0800

    企业认证完成增加公司与申请用户的关联

--- apps/recruit/apps.py
+++ apps/recruit/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class RecruitConfig(AppConfig):
     name = 'recruit'
     verbose_name = '招募令'
+
+    def ready(self):
+        import recruit.signals
--- apps/users/signals.py
+++ apps/users/signals.py
@@ -4,6 +4,7 @@ from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
 
 from users.models import ProfileExtendInfo
+from recruit.models import Company
 
 
 User = get_user_model()
@@ -18,3 +19,13 @@ def user_extend_create(sender, instance=None, created=False, **kwargs):
             ProfileExtendInfo.objects.create(user=instance)
         except Exception as e:
             logger.error('插入错误:{}'.format(e))
+
+
+@receiver(post_save, sender=Company)
+def user_admin_company(sender, instance=None, created=False, **kwargs):
+    # 审核通过
+    if instance.status == 30:
+        # 只有操作者还没有管理企业时才插入
+        if not instance.user.profileextendinfo.admin_company:
+            instance.user.profileextendinfo.admin_company = instance
+            instance.user.profileextendinfo.save()
