2ddfd4795e17f8be3ba684d893316cb0c3b26502
qiyuan <qiyuan@mercenary.com.cn>
2018-09-18 15:50:20 +0800

    同步模型;创建招募令接口

--- apps/recruit/serializers.py
+++ apps/recruit/serializers.py
@@ -36,18 +36,38 @@ class CompanyCreateSerializer(serializers.ModelSerializer):
 
 
 class PartTimeOrderCardSerializer(serializers.ModelSerializer):
-    address = AddressShortSerializer()
+    # address = AddressShortSerializer()
     class Meta:
         model = PartTimeOrderCard
-        fields = '__all__'
-        read_only_fields = ('id', 'work_time', 'status')
+        fields = ('address', 'start_time', 'end_time', 'enrol_count')
+        # read_only_fields = ('id', 'work_time', 'status')
 
 
 class PartTimeOrderCreateSerializer(serializers.ModelSerializer):
     cards = PartTimeOrderCardSerializer(many=True)
+    user = serializers.HiddenField(
+        default=serializers.CurrentUserDefault()
+    )
+
     class Meta:
         model = PartTimeOrder
-        fields = ('name', 'company', 'description', 'requirement', 'wages', 'settlement_method', 'deposit' ,'cards')
+        fields = ('user', 'company', 'name', 'description', 'requirement', 'wages', 'settlement_method', 'deposit' ,'cards')
+
+    def create(self, validated_data):
+        cards_data = validated_data.pop('cards')
+        order = PartTimeOrder.objects.create(**validated_data)
+        for card_data in cards_data:
+            PartTimeOrderCard.objects.create(recruit=order, **card_data)
+        return order
+
+    def validate(self, attrs):
+        if attrs.get('user').profileextendinfo.admin_company:
+            if attrs.get('user').profileextendinfo.admin_company != attrs.get('company'):
+                raise serializers.ValidationError('非法请求:企业管理员信息与用户信息不一致')
+            else:
+                return attrs
+        else:
+            raise serializers.ValidationError('非企业管理员不能创建招募令')
 
 
 class PartTimeOrderInfoSerializer(serializers.ModelSerializer):
