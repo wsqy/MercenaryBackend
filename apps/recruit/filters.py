bda849dafe3fcea2412d65f983aee954e59dc527
qiyuan <qiyuan@mercenary.com.cn>
2018-09-21 15:48:08 +0800

    招募令筛选由发布者变为公司

--- apps/recruit/filters.py
+++ apps/recruit/filters.py
@@ -9,8 +9,7 @@ class PartTimeOrderFilter(django_filters.rest_framework.FilterSet):
     """
     district = django_filters.Filter(field_name='cards', lookup_expr='address__district__name', help_text='县/区', label='县/区')
     city = django_filters.Filter(field_name='cards', lookup_expr='address__district__city__name', help_text='市', label='市')
-    publisher = django_filters.Filter(field_name='company', lookup_expr='user', help_text='发布者', label='发布者')
 
     class Meta:
         model = PartTimeOrder
-        fields = ['city', 'district', 'settlement_method', 'publisher']
+        fields = ['city', 'district', 'settlement_method', 'company']
