dd9b7ad64d90a339a2e6290874e54860986e30df
qiyuan <qiyuan@mercenary.com.cn>
2018-10-09 19:22:51 +0800

    Q函数引入不正常

--- apps/recruit/views.py
+++ apps/recruit/views.py
@@ -1,4 +1,5 @@
 from django.utils import timezone
+from django.db.models import Q
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, CreateModelMixin, DestroyModelMixin
